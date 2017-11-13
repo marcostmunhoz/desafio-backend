775345710cd588774e9323ad33b7d45efaeaa833
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2017-11-13 19:32:50 -0200

    Pequenas alterações e adição dos comentários relevantes

--- src/com/desafio/Main.java
+++ src/com/desafio/Main.java
@@ -9,25 +9,30 @@ import java.sql.DriverManager;
 public class Main {
     private static final String USUARIO = "root";
     private static final String SENHA = "";
-    private static final String URL = "jdbc:mysql://localhost:3306/banco_desafio_backend";
-    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String BANCO = "jdbc:mysql://localhost:3306/banco_desafio_backend";
 
-    private static String repeat(String repeatChar, int count) {
-        return String.format("%0" + count + "d", 0).replace("0", repeatChar);
+    // Metodo auxiliar para geração de "linhas" para a tabela
+    // Executa um loop n vezes, adicionando um caracter fornecido a uma string temporária, posteriormente retornando a mesma
+    private static String repeat(Character repeatChar, int count) {
+        String repeated = "";
+        for (int i = 0; i < count; i++) {
+            repeated += repeatChar;
+        }
+        return repeated;
     }
 
     public static void main(String[] args) {
         try {
-            Class.forName(DRIVER);
-            Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            Class.forName("com.mysql.jdbc.Driver"); // Chamada para o driver do mysql
+            Connection conexao = DriverManager.getConnection(BANCO, USUARIO, SENHA); //Conexão com o banco de dados
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); //Entrada de dados
             int id, ativo, quantidade;
             String cpf_cnpj, nome;
             double valor;
 
-            if (args.length == 0) {
+            if (args.length == 0) { // Executa quando não forem fornecidos os parametros de linha de comando
                 System.out.print("Seja bem vindo ao sistema.\nQuantos registros você deseja inserir? ");
-                quantidade = Integer.parseInt(reader.readLine());
+                quantidade = Integer.parseInt(reader.readLine()); //Lê o dado do reader
                 if (quantidade > 0) {
                     for (int i = 0; i < quantidade; i++) {
                         System.out.print("Identificação do cliente: ");
@@ -36,7 +41,7 @@ public class Main {
                         cpf_cnpj = reader.readLine();
                         System.out.print("Nome do cliente: ");
                         nome = reader.readLine();
-                        System.out.print("Consumidor ativo? (s/n): ");
+                        System.out.print("Ativo? (s/n): ");
                         if (reader.readLine().contains("s")) {
                             ativo = 1;
                         } else {
@@ -44,11 +49,11 @@ public class Main {
                         }
                         System.out.print("Valor total: ");
                         valor = Double.parseDouble(reader.readLine());
-                        PreparedStatement id_stmt = conexao.prepareStatement("SELECT COUNT(id_customer) FROM tb_customer_account WHERE id_customer = ?");
-                        id_stmt.setInt(1, id);
-                        ResultSet id_result = id_stmt.executeQuery();
-                        id_result.next();
-                        if (id_result.getInt(1) == 0) {
+                        PreparedStatement id_stmt = conexao.prepareStatement("SELECT COUNT(id_customer) FROM tb_customer_account WHERE id_customer = ?"); // Verifica se já existe o id informado
+                        id_stmt.setInt(1, id); // Alimenta a query com o parametro 'id'
+                        ResultSet id_result = id_stmt.executeQuery(); // Executa a query e registra num resultset
+                        id_result.next(); // Avança para a primeira linha (unica, no caso)
+                        if (id_result.getInt(1) == 0) { // Checa se o valor da primeira coluna (unica, novamente) é = 0, ou seja, não existem registros com o mesmo id
                             PreparedStatement insert_stmt = conexao.prepareStatement("INSERT INTO tb_customer_account VALUES (?, ?, ?, ?, ?)");
                             insert_stmt.setInt(1, id);
                             insert_stmt.setString(2, cpf_cnpj);
@@ -62,7 +67,7 @@ public class Main {
                         System.out.println();
                     }
                 }
-            } else if (args.length == 5) {
+            } else if (args.length == 5) { // Executa quando os 5 parametros estão presentes (automatiza a entrada de dados)
                 PreparedStatement id_stmt = conexao.prepareStatement("SELECT COUNT(id_customer) FROM tb_customer_account WHERE id_customer = ?");
                 id_stmt.setInt(1, Integer.parseInt(args[0]));
                 ResultSet id_result = id_stmt.executeQuery();
@@ -86,15 +91,17 @@ public class Main {
             System.out.println("\nMédia = " + Double.toString(soma) + "\n");
             ResultSet select_result = conexao.prepareStatement(
                     "SELECT * FROM tb_customer_account WHERE vl_total > 560 AND id_customer BETWEEN 1500 AND 2700 ORDER BY vl_total DESC").executeQuery();
-            System.out.println("+" + repeat("=", 94) + "+");
-            System.out.format("| %4s | %14s | %45s | %6s | %11s |\n", "ID", "CPF/CNPJ", "Nome", "Ativo?", "Valor total");
-            System.out.println("+" + repeat("=", 94) + "-");
-            while (select_result.next()) {
+            System.out.println("+" + repeat('=', 94) + "+"); // Chamada a função auxiliar para gerar a linha superior da tabela
+            System.out.format("| %4s | %14s | %45s | %6s | %11s |\n", "ID", "CPF/CNPJ", "Nome", "Ativo?", "Valor total"); // Realiza a formatação das "colunas" da tabela
+            // Separando pela quantidade de caracteres para cada coluna
+            System.out.println("+" + repeat('-', 94) + "+");
+            while (select_result.next()) { // Loop pelas linhas do resultset
                 System.out.format("| %4d | %14s | %45s | %6s | %11.2f |\n", select_result.getInt(1), select_result.getString(2),
                         select_result.getString(3), select_result.getInt(4) == 1 ? "sim" : "não", select_result.getDouble(5));
             }
-            System.out.println("+" + repeat("=", 94) + "+");
-        } catch (Exception ex) {
+            System.out.println("+" + repeat('=', 94) + "+");
+            conexao.close();
+        } catch (Exception ex) { //Tratamento de exceção
            System.out.println("Erro encontrado: " + ex.toString());
         }
     }
